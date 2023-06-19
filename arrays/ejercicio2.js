ef28fa48554a198e855c52cafe01cc339bdbe511
Fernanda Paredes <fernandaparedes0@gmail.com>
2023-06-19 12:26:37 -0400

    cambios_commit

--- arrays/ejercicio2.js
+++
@@ -1,6 +0,0 @@
-/*Let amigos = ["pedro","maria","joan","pili"];1- queremos que escriba "mis amigos son maria y joan.” con el siguiente array , haz que muestre la frase por consola.*/
- 
-let amigos = ["pedro","maria","joan","pili"];
-
-let extraer = amigos.slice(1, 3);
- console.log(`Mis amigos son ${extraer}`);
